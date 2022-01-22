b31839f99077d0a41e154484d93d1e091ff3fbc8
Kigyós János <cycool89@gmail.com>
2022-01-22 16:39:39 +0100

    Add tsconfig and some npm scripting

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,6 +1,12 @@
-export class Main {
-    public static main() {
-
+class Main {
+    public static main(): void {
+        /*
+        const main = document.getElementById("app-main");
+        if (main) {
+            main.innerText = 'Hello world!';
+        }
+        */
     }
 }
+
 Main.main();
